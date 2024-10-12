4277b80bac7db7f7f9013778877c954860b9f8a3
Akash <akashgawarecontact@gmail.com>
2024-10-12 13:43:49 +0530

    Added input in Calculator

---
+++ GitGitHub/src/com/Demo/Calculator.java
@@ -0,0 +1,22 @@
+package com.Demo;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	int add(int a,int b) {
+		
+		return a+b;
+	}
+int sub(int a,int b) {
+		
+		return a-b;
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	}
+
+}
