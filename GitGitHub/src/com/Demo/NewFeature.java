db4d6b87cf5eb1b2176d5be0f3038700a910a53b
Akash <akashgawarecontact@gmail.com>
2024-10-13 16:31:31 +0530

    Added new Feature

---
+++ GitGitHub/src/com/Demo/NewFeature.java
@@ -0,0 +1,10 @@
+package com.Demo;
+
+public class NewFeature {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Added new Feature using this class");
+	}
+
+}
